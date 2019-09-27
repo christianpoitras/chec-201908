91359e07bb338e1e20f9495465bdab3481fb76eb
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-09-27 16:52:15 -0400

    Adds setup for python.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ChecSeqTools",
+    version="0.1-SNAPSHOT",
+    packages=find_packages(),
+    author="Christian Poitras",
+    author_email="christian.poitras@ircm.qc.ca",
+    description="Tools to analyze ChEC-seq data",
+    keywords="bioinformatics, ChEC-seq",
+    url="https://github.com/francoisrobertlab/chec-201908",
+    license='GNU General Public License version 3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License version 3'
+    ],
+    install_requires=[
+        'click>=7.0',
+        'pandas>=0.25.1'
+        'seqtools @ https://github.com/francoisrobertlab/seqtools/tarball/v0.1.1',
+    ],
+    entry_points={
+        'console_scripts': [
+            'prepgenecov = checseqtools.PrepareGenomeCoverage:main',
+            'fullanalysis = checseqtools.FullAnalysis:main'
+        ]
+    }
+)
