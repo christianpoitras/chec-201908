4a6296025d5b99e79f3db4c9c543beabbe10262b
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-12-03 09:48:54 -0500

    Use HEAD revision of seqtools.

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ChecSeqTools",
-    version="0.1-SNAPSHOT",
+    name='ChecSeqTools',
+    version='0.1-SNAPSHOT',
     packages=find_packages(),
-    author="Christian Poitras",
-    author_email="christian.poitras@ircm.qc.ca",
-    description="Tools to analyze ChEC-seq data",
-    keywords="bioinformatics, ChEC-seq",
-    url="https://github.com/francoisrobertlab/chec-201908",
+    author='Christian Poitras',
+    author_email='christian.poitras@ircm.qc.ca',
+    description='Tools to analyze ChEC-seq data',
+    keywords='bioinformatics, ChEC-seq',
+    url='https://github.com/francoisrobertlab/chec-201908',
     license='GNU General Public License version 3',
     classifiers=[
         'License :: OSI Approved :: GNU General Public License version 3'
@@ -17,7 +17,7 @@ setup(
     install_requires=[
         'click>=7.0',
         'pandas>=0.25.0',
-        'seqtools@http://github.com/francoisrobertlab/seqtools/tarball/0.1.4'
+        'seqtools@http://github.com/francoisrobertlab/seqtools/tarball/master'
     ],
     entry_points={
         'console_scripts': [
