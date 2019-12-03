273bd55112fa30a0f62435ccc8809d6ef29c6652
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-12-03 09:53:52 -0500

    Fixes module for Calcul Canada servers.

--- chec-201908.lua
+++ chec-201908.lua
@@ -10,10 +10,12 @@ This module sets the following environment variables:
 This module loads the following modules and their requirements:
     - python/3.7.4
     - bwa/0.7.17
+    - bowtie2/2.3.4.3
     - samtools/1.9
     - bedtools/2.27.1
     - kentutils/20180716
-    - r/3.6.0
+    - vap
+    - plot2do
 ]])
 
 whatis("Version: 1.0.0")
@@ -25,11 +27,12 @@ always_load("nixpkgs/16.09")
 always_load("gcc/7.3.0")
 always_load("python/3.7.4")
 always_load("bwa/0.7.17")
+always_load("bowtie2/2.3.4.3")
 always_load("samtools/1.9")
 always_load("bedtools/2.27.1")
 always_load("kentutils/20180716")
-always_load("r/3.6.0")
 always_load("vap")
+always_load("plot2do")
 
 local base = "~/projects/def-robertf/chec-201908"
 prepend_path("PATH", pathJoin(base,"bash"))
