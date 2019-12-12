b979d1fa74217d1f0aba0461cc3a22259d8f0b8c
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-12-12 11:35:22 -0500

    Fixes DyadPositionFinder errors.

--- checseqtools/DyadPositionFinder.py
+++ checseqtools/DyadPositionFinder.py
@@ -15,26 +15,28 @@ NEGATIVE_STRAND = -1
 @click.option('--signal', '-s', type=click.Path(exists=True), default='signal.bw',
               help='Dryad signal as a bigWig or bigBed file.')
 @click.option('--dryad', '-d', type=int, default=2,
-              help='Dryad index.')
+              help='Dryad index. Must be 2 right now.')
 @click.option('--output', '-o', type=click.Path(), default='genes-out.txt',
               help='Output file.')
 def main(genes, signal, dryad, output):
     '''Prepare BED file used for genome coverage on samples.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     if dryad != 2:
-        print >> sys.stderr, 'dryad parameter must be 2'
+        print >> sys.stderr, 'right now, dryad parameter must be 2'
         sys.exit(1)
-    genes_info = pd.read_csv(genes, header=None, sep='\t', comment='#')
+    genes_info = pd.read_csv(genes, sep='\t', comment='#')
     bw = pbw.open(signal)
-    for gene_info in genes_info:
-        chromosome = gene_info[1]
-        strand = gene_info[4] == NEGATIVE_STRAND
-        first_nucleosome = gene_info[7]
-        start = first_nucleosome + (141 if strand else -141)
-        end = start + 41 if strand else start - 41;
-        next_nucleosome = highest_signal(bw, chromosome, start, end)
-        gene_info[11] = next_nucleosome[0]
-    pd.to_csv(output, header=None, sep='\t', comment='#')
+    next_nucleosomes = []
+    for index, columns in genes_info.iterrows():
+        chromosome = columns[1]
+        strand = columns[4] == NEGATIVE_STRAND
+        first_nucleosome = int(columns[7])
+        start = first_nucleosome + (-140 if strand else 141)
+        end = start - 41 if strand else start + 41;
+        next_nucleosome = highest_signal(bw, chromosome, min(start, end), max(start, end))
+        next_nucleosomes.append(next_nucleosome[0])
+    genes_info['+' + str(dryad) + ' nucleosome'] = next_nucleosomes
+    genes_info.to_csv(output, sep='\t')
 
 
 def highest_signal(bw, chromosome, start, end):
