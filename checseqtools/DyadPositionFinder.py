06662d1fb7acc68f0eaaf55d12a60a4af60ae295
Christian Poitras <christian.poitras@ircm.qc.ca>
2019-12-17 14:09:22 -0500

    Fixes dyad name and allow selection of distance between dyads.

--- checseqtools/DyadPositionFinder.py
+++ checseqtools/DyadPositionFinder.py
@@ -13,29 +13,34 @@ NEGATIVE_STRAND = -1
 @click.option('--genes', '-g', type=click.Path(exists=True), default='genes.txt',
               help='Genes information.')
 @click.option('--signal', '-s', type=click.Path(exists=True), default='signal.bw',
-              help='Dryad signal as a bigWig or bigBed file.')
-@click.option('--dryad', '-d', type=int, default=2,
-              help='Dryad index. Must be 2 right now.')
+              help='Dyad signal as a bigWig or bigBed file.')
+@click.option('--dyad', '-i', type=int, default=2,
+              help='Dyad index. Must be 2 right now.')
+@click.option('--mind', '-d', type=int, default=141,
+              help='Minimum distance from previous dyad.')
+@click.option('--maxd', '-D', type=int, default=181,
+              help='Maximum distance from previous dyad.')
 @click.option('--output', '-o', type=click.Path(), default='genes-out.txt',
               help='Output file.')
-def main(genes, signal, dryad, output):
+def main(genes, signal, dyad, mind, maxd, output):
     '''Prepare BED file used for genome coverage on samples.'''
     logging.basicConfig(filename='debug.log', level=logging.DEBUG, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-    if dryad != 2:
-        print >> sys.stderr, 'right now, dryad parameter must be 2'
+    if dyad != 2:
+        print >> sys.stderr, 'right now, dyad parameter must be 2'
         sys.exit(1)
     genes_info = pd.read_csv(genes, sep='\t', comment='#')
     bw = pbw.open(signal)
+    diffd = maxd - mind
     next_nucleosomes = []
     for index, columns in genes_info.iterrows():
         chromosome = columns[1]
         strand = columns[4] == NEGATIVE_STRAND
         first_nucleosome = int(columns[7])
-        start = first_nucleosome + (-140 if strand else 141)
-        end = start - 41 if strand else start + 41;
+        start = first_nucleosome + (-(mind - 1) if strand else mind)
+        end = start - diffd if strand else start + diffd;
         next_nucleosome = highest_signal(bw, chromosome, min(start, end), max(start, end))
         next_nucleosomes.append(next_nucleosome[0])
-    genes_info['+' + str(dryad) + ' nucleosome'] = next_nucleosomes
+    genes_info['+' + str(dyad) + ' nucleosome'] = next_nucleosomes
     genes_info.to_csv(output, sep='\t')
 
 
